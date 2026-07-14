f9cc0b7400a149ce113d410b800941e1c62b2c42
rimedra <rimedra1954@gmail.com>
2026-07-14 12:18:18 -0500

    option selecting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,12 @@ using UnityEngine;
 
 public static class GameManager
 {
+    #if CLIENT
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public static ClientBehaviour clientBehaviour;
     // public static Func<CardStruct,bool> ValidCard;
     public static PlayPile playPile;
-
-    public static Unity.Networking.Transport.NetworkEndpoint networkEndpoint = Unity.Networking.Transport.NetworkEndpoint.LoopbackIpv4.WithPort(port);
-
     public static string username="";
-    public const int port =7777;
-    //high to low
     public static CardValue[] sortOrder = new []{
         CardValue.Joker,
         CardValue.Ace,
@@ -37,6 +33,13 @@ public static class GameManager
         if(cardStruct.value==CardValue.Blank) return Resources.Load<Sprite>($"Card Art/Back/0");
         return Resources.Load<Sprite>($"Card Art/{cardStruct.value}/0");
     }
+    #endif
+    public static Unity.Networking.Transport.NetworkEndpoint networkEndpoint = Unity.Networking.Transport.NetworkEndpoint.LoopbackIpv4.WithPort(port);
+
+    
+    public const int port =7777;
+    //high to low
+    
 
 
 }
